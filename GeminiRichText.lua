1038f6fdf51cf0c1ff9e47a6e9d147df6fd4507f
Togglebutton <bpetri20@gmail.com>
2014-06-23 19:18:07 -0500

    Style Editor
    Added Style Editor
    Updated README with new method information.
    Updated parsing code some.

--- GeminiRichText.lua
+++ GeminiRichText.lua
@@ -1,13 +1,16 @@
-﻿--================================================================================================
+--================================================================================================
 --
 --										GeminiRichText
--- 				Apollo Package for adding Rich Text editing to an addon simply.
---								
+--
+--			An Apollo Package for dealing with special text shown in MLWindow UI windows.
+--
+--
 --================================================================================================
- --[[
+
+--[[
 The MIT License (MIT)
 
-Copyright (c) 2014 Wildstar NASA
+Copyright (c) 2014 2014 Wildstar NASA
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -29,33 +32,50 @@ THE SOFTWARE.
 ]]
 
 require "Window"
- 
- local MAJOR, MINOR = "GeminiRichText", 1
-local APkg = Apollo.GetPackage(MAJOR)
+-----------------------------------------------------------------------------------------------
+-- GeminiRichText Local Functions
+-----------------------------------------------------------------------------------------------
 
+local function SetDDSelectByName(self, wndButton, strName)
+	local tButtonList = wndButton:FindChild("ddList"):GetChildren()
+	for i,v in pairs(tButtonList) do
+		if v:GetName() == "btn_"..strName then
+			wndButton:FindChild("ddList"):SetRadioSelButton("DDList", v)
+			wndButton:SetText(strName)
+			return v
+		end
+	end
+end
+
+local function UpdateSampleText(wndStyleEditor)
+	local strFont = wndStyleEditor:FindChild("btn_DDFont"):GetData()
+	local strAlign = wndStyleEditor:FindChild("btn_DDAlign"):GetData()
+	local strColor = wndStyleEditor:FindChild("btn_Color"):GetData()
+	
+	local tStyleData = wndStyleEditor:GetData()
+	local xmlStyleDoc = string.format("<P Font=%q Align=%q TextColor=%q>Sample %s style</P>", strFont, strAlign, strColor, tStyleData[2].tag)
+	local wndSample = wndStyleEditor:FindChild("wnd_Sample")
+	
+	wndSample:SetAML(xmlStyleDoc)	
+end
+-----------------------------------------------------------------------------------------------
+-- GeminiRichText Module Definition
+-----------------------------------------------------------------------------------------------
+
+local MAJOR, MINOR = "GeminiRichText", 1
+-- Get a reference to the package information if any
+local APkg = Apollo.GetPackage(MAJOR)
+-- If there was an older version loaded we need to see if this is newer
 if APkg and (APkg.nVersion or 0) >= MINOR then
-	return
+	return -- no upgrade needed
 end
 
 local GeminiRichText = APkg and APkg.tPackage or {}
 
-function GeminiRichText:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self 
+-----------------------------------------------------------------------------------------------
+-- GeminiRichText OnLoad and Instancing
+-----------------------------------------------------------------------------------------------
 
-    -- initialize variables here
-
-    return o
-end
-
-function GeminiRichText:Init()
-	local tDependencies = {
-		-- "UnitOrPackageName",
-	}
-    Apollo.RegisterPackage(GeminiRichText:new(), MAJOR, MINOR, tDependencies)
-end
- 
 function GeminiRichText:OnLoad()
 	local strPrefix = Apollo.GetAssetFolder()
 	local tToc = XmlDoc.CreateFromFile("toc.xml"):ToTable()
@@ -67,6 +87,115 @@ function GeminiRichText:OnLoad()
 		end
 	end
 	self.xmlDoc = XmlDoc.CreateFromFile(strPrefix.."GeminiRichText.xml")
+end
+
+function GeminiRichText:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self 
+	return o
+end
+
+-----------------------------------------------------------------------------------------------
+-- GeminiRichText Internal Methods
+-----------------------------------------------------------------------------------------------
+function GeminiRichText:OnEditHistoryBoxChanged( wndHandler, wndControl, strText )
+	local wndCounter = wndControl:GetParent():FindChild("wnd_CharacterCount")
+	if wndCounter:IsShown() ~= true then return end
+	local nCharacterLimit = wndCounter:GetData().nCharacterLimit
+	local nCharacterCount = string.len(wndControl:GetText())
+	wndCounter:SetText(tostring(nCharacterLimit - nCharacterCount))
+end
+
+function GeminiRichText:InsertTag(wndHandler, wndControl)
+	local wndEditBox = wndControl:GetParent():FindChild("input_s_Text")
+	local tagType = string.sub(wndControl:GetName(), 5)
+	local tSelected = wndEditBox:GetSel()
+	
+	if (tSelected.cpEnd - tSelected.cpBegin ) > 0 then
+		local strSelectedText = string.sub(wndEditBox:GetText(), tSelected.cpBegin, tSelected.cpEnd)
+		wndEditBox:InsertText(string.format("\{%s\}%s\{/%s\}",tagType, strSelectedText, tagType))
+	elseif tagType == "hr" then
+		wndEditBox:InsertText(string.format("\{%s\}",tagType))
+	else
+		wndEditBox:InsertText(string.format("\{%s\}\{/%s\}",tagType, tagType))
+		wndEditBox:SetSel(string.len(wndEditBox:GetText()) - (string.len(tagType) + 3), string.len(wndEditBox:GetText()) - (string.len(tagType) + 3))
+	end
+	
+end
+
+function GeminiRichText:DDClick(wndHandler, wndControl)
+	local bDDListShown = wndControl:FindChild("ddList"):IsShown()
+	wndControl:FindChild("ddList"):Show(not (bDDListShown))
+end
+
+function GeminiRichText:DDSelect(wndHandler, wndControl)
+	local wndButton = wndControl:GetParent():GetParent()
+	local wndStyleEditor = wndControl:GetParent():GetParent():GetParent()
+	local strSelection = string.sub(wndControl:GetName(), 5)
+	wndButton:SetData(strSelection)
+	wndButton:SetText(wndControl:GetText())
+	wndControl:GetParent():Show(false)
+	UpdateSampleText(wndStyleEditor)
+end
+
+function GeminiRichText:StyleDblClick(wndHandler, wndControl, iRow, iCol)
+	local wndStyleEditor = wndControl:FindChild("wnd_StyleOptionsEditor")
+	local tStyleData = {iRow, wndControl:GetCellLuaData(iRow, 1)}
+	wndStyleEditor:SetData(tStyleData)
+	wndStyleEditor:Show(true)
+end
+
+function GeminiRichText:StyleEditorShow(wndHandler, wndControl)
+	if wndHandler ~= wndControl then return end
+	local tStyleData = wndControl:GetData()
+	local btnFontDD = wndControl:FindChild("btn_DDFont")
+	local btnAlignDD = wndControl:FindChild("btn_DDAlign")
+	local btnColor = wndControl:FindChild("btn_Color")
+	local wndSample = wndControl:FindChild("wnd_Sample")
+	
+	btnFontDD:SetData(tStyleData[2].font)
+	SetDDSelectByName(self, btnFontDD, tStyleData[2].font)
+	
+	btnAlignDD:SetData(tStyleData[2].align)
+	SetDDSelectByName(self, btnAlignDD, tStyleData[2].align)
+	
+	btnColor:SetData(tStyleData[2].color)
+	btnColor:FindChild("swatch"):SetBGColor(tStyleData[2].color)
+	local strSampleText = "<P Font=%q Align=%q TextColor=%q>Sample %s style.</P>"
+	wndSample:SetAML(string.format(strSampleText, tStyleData[2].font, tStyleData[2].align, tStyleData[2].color, tStyleData[2].tag))
+end
+
+function GeminiRichText:StyleEditorOK(wndHandler, wndControl)
+	local wndStyleOptions = wndControl:GetParent()
+	local strFont = wndStyleOptions:FindChild("btn_DDFont"):GetData()
+	local strAlign = wndStyleOptions:FindChild("btn_DDAlign"):GetData()
+	local strColor = wndStyleOptions:FindChild("btn_Color"):GetData()
+	local tDat = wndStyleOptions:GetData()
+	local iRow = tDat[1]
+	local tStyleData = tDat[2]
+	local wndStyleList = wndStyleOptions:GetParent()
+	tStyleData.align = strAlign
+	tStyleData.font = strFont
+	tStyleData.color = strColor
+	wndStyleList:SetCellLuaData(iRow, 1, tStyleData)
+	
+	local xmlStyleDoc = string.format("<P Font=%q Align=%q TextColor=%q>Style: %s</P>", strFont, strAlign, strColor, tStyleData.tag)
+	wndStyleList:SetCellDoc(iRow, 1, xmlStyleDoc)
+	
+	wndStyleOptions:Show(false,true)
+end
+-----------------------------------------------------------------------------------------------
+-- GeminiRichText External Methods
+-----------------------------------------------------------------------------------------------
+-- Edit Box
+function GeminiRichText:SetText(wndMarkup, strText)
+	wndMarkup:FindChild("input_s_Text"):SetText(strText)
+end
+
+function GeminiRichText:GetText(wndMarkup)
+	local strText = wndMarkup:FindChild("input_s_Text"):GetText()
+	return strText
 end
 
 function GeminiRichText:CreateMarkupEditControl(wndHost, strSkin, tProperties, tAddon)
@@ -140,41 +269,122 @@ function GeminiRichText:CreateMarkupEditControl(wndHost, strSkin, tProperties, t
 	return wndMarkup
 end
 
-function GeminiRichText:OnEditHistoryBoxChanged( wndHandler, wndControl, strText )
-	local wndCounter = wndControl:GetParent():FindChild("wnd_CharacterCount")
-	if wndCounter:IsShown() ~= true then return end
-	local nCharacterLimit = wndCounter:GetData().nCharacterLimit
-	local nCharacterCount = string.len(wndControl:GetText())
-	wndCounter:SetText(tostring(nCharacterLimit - nCharacterCount))
+-- Style Edit
+function GeminiRichText:CreateMarkupStyleEditor(wndHost, tStyleData)
+	-- wndHost = place holder window, used to get Window Name, Anchors and Offsets, and Parent
+	if wndHost == nil then Print("You must supply a valid window for argument #1."); return end
+	
+	if tStyleData == nil then
+		tStyleData = {
+			{tag = "h1", font = "CRB_Interface14_BBO", color = "ffffffff", align = "Center"},
+			{tag = "h2", font = "CRB_Interface12_BO", color = "ffffffff", align = "Left"},
+			{tag = "h3", font = "CRB_Interface12_I", color = "ffffffff", align = "Left"},
+			{tag = "p", font = "CRB_Interface12", color = "ffaaaaaa", align = "Left"},
+			{tag = "li", font = "CRB_Interface12", color = "ffaaaaaa", align = "Left", bullet = "●", indent = "  "},
+		}
+	end
+
+	local fLeftAnchor, fTopAnchor, fRightAnchor, fBottomAnchor = wndHost:GetAnchorPoints()
+	local fLeftOffset, fTopOffset, fRightOffset, fBottomOffset = wndHost:GetAnchorOffsets()
+	local strName = wndHost:GetName()
+	local wndParent = wndHost:GetParent()
+	local wndStyles
+
+	wndHost:Destroy()
+
+	wndStyles = Apollo.LoadForm(self.xmlDoc, "StyleEditorForm", wndParent, self)
+	local btnFontDD = wndStyles:FindChild("btn_DDFont")
+	
+	local wndDDList = btnFontDD:FindChild("ddList")
+	local tGameFontList = Apollo.GetGameFonts()
+	local tFontList = {}
+	
+	for i, v in pairs(tGameFontList) do
+		if string.find(v.name, "CRB_Interface%d?%d") then
+			table.insert(tFontList, v.name)
+		elseif string.find(v.name, "CRB_Header%d?%d") then
+			table.insert(tFontList, v.name)
+		end
+	end		
+	table.sort(tFontList)
+	-- from hhtp://lua-users.org/wiki/TableUtils
+	
+	local function table_count(tt, item)
+	  local count
+	  count = 0
+	  for ii,xx in pairs(tt) do
+		if item == xx then count = count + 1 end
+	  end
+	  return count
+	end
+	
+	local function table_unique(tt)
+	  local newtable
+	  newtable = {}
+	  for ii,xx in ipairs(tt) do
+		if(table_count(newtable, xx) == 0) then
+		  newtable[#newtable+1] = xx
+		end
+	  end
+	  return newtable
+	end
+	
+	tFontList = table_unique(tFontList)
+	
+	for i, v in pairs(tFontList) do
+		local wnd = Apollo.LoadForm(self.xmlDoc, "DropDownItemForm", wndDDList, self)
+		wnd:SetText(v)
+		wnd:SetName("btn_"..v)
+		wnd:SetFont(v)
+	end
+	
+	wndDDList:ArrangeChildrenVert()
+	wndDDList:Show(false, true)
+		
+	wndStyles:FindChild("wnd_StyleOptionsEditor:btn_DDAlign:ddList"):Show(false, true)
+	
+	wndStyles:SetAnchorPoints(fLeftAnchor, fTopAnchor, fRightAnchor, fBottomAnchor)
+	wndStyles:SetAnchorOffsets(fLeftOffset, fTopOffset, fRightOffset, fBottomOffset)
+	wndStyles:SetName(strName)
+	for i,v in pairs(tStyleData) do
+		local iCurrRow = wndStyles:AddRow("")
+		wndStyles:SetCellLuaData(iCurrRow, 1, v)
+		local xmlStyleDoc = string.format("<P Font=%q Align=%q TextColor=%q>Style: %s</P>", v.font, v.align,  v.color, v.tag)
+		wndStyles:SetCellDoc(iCurrRow, 1, xmlStyleDoc)
+	end
+	
+	wndStyles:FindChild("wnd_StyleOptionsEditor"):Show(false)
+	
+	return wndStyles, wndStyles:FindChild("wnd_StyleOptionsEditor:btn_Color")
+	-- returns window, and color button
 end
 
-function GeminiRichText:InsertTag(wndHandler, wndControl)
-	local wndEditBox = wndControl:GetParent():FindChild("input_s_Text")
-	local tagType = string.sub(wndControl:GetName(), 5)
-	local tSelected = wndEditBox:GetSel()
-	
-	if (tSelected.cpEnd - tSelected.cpBegin ) > 0 then
-		local strSelectedText = string.sub(wndEditBox:GetText(), tSelected.cpBegin, tSelected.cpEnd)
-		wndEditBox:InsertText(string.format("\{%s\}%s\{/%s\}",tagType, strSelectedText, tagType))
-	else
-		wndEditBox:InsertText(string.format("\{%s\}\{/%s\}",tagType, tagType))
-		wndEditBox:SetSel(string.len(wndEditBox:GetText()) - (string.len(tagType) + 3), string.len(wndEditBox:GetText()) - (string.len(tagType) + 3))
+function GeminiRichText:GetStyleTable(wndStyleEditor)
+	local nStyles = wndStyleEditor:GetRowCount()
+	local tStyles = {}
+	for i = 1, nStyles do
+		local tCurrStyle = wndStyleEditor:GetCellLuaData(i, 1)
+		table.insert(tStyles, tCurrStyle)
+	end
+	return tStyles
+end
+
+function GeminiRichText:SetStyleTable(wndStyleEditor, tStyleData)
+	wndStyleEditor:DeleteAll()
+	for i,v in pairs(tStyleData) do
+		local iCurrRow = wndStyleEditor:AddRow("")
+		wndStyleEditor:SetCellLuaData(iCurrRow, 1, v)
+		local xmlStyleDoc = string.format("<P Font=%q Align=%q TextColor=%q>Style: %s</P>", v.font, v.align,  v.color, v.tag)
+		wndStyleEditor:SetCellDoc(iCurrRow, 1, xmlStyleDoc)
 	end
 	
 end
 
-function GeminiRichText:SetText(wndMarkup, strText)
-	wndMarkup:FindChild("input_s_Text"):SetText(strText)
-end
-
-function GeminiRichText:GetText(wndMarkup)
-	local strText = wndMarkup:FindChild("input_s_Text"):GetText()
-	return strText
-end
-
+-- Parsing function
 function GeminiRichText:ParseMarkup(strText, tMarkupStyles)
 	-- strText = the text to be parsed
-	-- tMarkupStyles = 
+	-- tMarkupStyles = Markup styles the editor should pass.
+	strText = FixXMLString(strText)
 	
 	if tMarkupStyles == nil then
 		tMarkupStyles = {
@@ -183,18 +393,30 @@ function GeminiRichText:ParseMarkup(strText, tMarkupStyles)
 			{tag = "h3", font = "CRB_Interface12_I", color = "ffffffff", align = "Left"},
 			{tag = "p", font = "CRB_Interface12", color = "ffaaaaaa", align = "Left"},
 			{tag = "li", font = "CRB_Interface12", color = "ffaaaaaa", align = "Left", bullet = "●", indent = "  "},
-		},
+		}
 	end
 	
-	strText = string.gsub(strText, "\n", "<BR />")
+	local tP
+	
+	for i,v in pairs(tMarkupStyles) do
+		if v.tag == "p" then
+			tP = v
+		end
+	end
+	
+	local tPtag -- to save the main text tag for later
+	strText = string.gsub(strText, "\n\n", string.format("<P Font=%q TextColor=\"00ffffff\">BlankLine</P>", tP.font))
+	strText = string.gsub(strText, "{hr}", string.format("<P Font=%q TextColor=%q Align=%q>――――――――――――――――――――</P>", tP.font, tP.color, "Center"))
 	for i, v in pairs(tMarkupStyles) do
-		local strOpenTag = "\{"..v.tag.."\}"
-		local strCloseTag = "\{\/"..v.tag.."\}"
-		local strSubTagOpen= [[<P Font="]]..v.font..[[" Align="]]..v.align..[[" TextColor="]]..v.color..[[">]]
+		local strOpenTag = string.format("{%s}",v.tag)
+		local strCloseTag = string.format("{/%s}",v.tag)
+		local strSubTagOpen = string.format("<P Font=%q Align=%q TextColor=%q>", v.font, v.align, v.color)
 		local strSubTagClose = "</P>"
 
-		if v.bullet ~= nil then
-			strSubTagOpen= strSubTagOpen..(v.indent or "")..v.bullet.." "
+		if v.tag == "li" then
+			local bullet = v.bullet or "●"
+			local indent = v.indent or "  "
+			strSubTagOpen = string.format("%s%s%s%s", strSubTagOpen, indent, bullet, indent)
 		end
 		
 		if string.find(strText, strOpenTag) then
@@ -205,11 +427,17 @@ function GeminiRichText:ParseMarkup(strText, tMarkupStyles)
 			
 			strText = string.gsub(strText, strCloseTag, strSubTagClose)
 		end
+		
+		if v.tag == "p" then
+			tPtag = v
+		end
 	end
+	
+	--[[
 	local _, nOpenCount = string.gsub(strText, "<P", "")
 	local _, nCloseCount = string.gsub(strText, "/P>", "")
 	
-	--[[if nOpenCount < nCloseCount then
+	if nOpenCount < nCloseCount then
 		local nCloseTagsNeeded = nOpenCount - nCloseCount
 		for i = 1, nCloseTagsNeeded do
 			strText = strText.."</P>"
@@ -219,8 +447,14 @@ function GeminiRichText:ParseMarkup(strText, tMarkupStyles)
 		for i = 1, nCloseTagsNeeded do
 			strText = "<P>"..strText
 		end
-	end]]
+	end
+	]]
+	strText = string.format("<P Font=\"%s\" TextColor=\"%s\" Align=\"%s\">%s</P>", tPtag.font, tPtag.color, tPtag.align, strText)
 	return strText
 end
 
-GeminiRichText:Init()
+
+-----------------------------------------------------------------------------------------------
+-- GeminiRichText Registration
+-----------------------------------------------------------------------------------------------
+Apollo.RegisterPackage(GeminiRichText:new(), MAJOR, MINOR, {})
--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 GeminiRichText
 ============
 
-An Apollo Package for adding a rich text markup window to yoru addon, and methods for parsing text for use in an MLWindow.
-
+An Apollo Package for adding a rich text markup window and a style editor window to your addon. GeminiRichText also provides a method for parsing text for use in an MLWindow.
 
 
 ## To Call: ##
 GeminiRichText = Apollo.GetPackage("GeminiRichText").tPackage
 
 
-## GeminiRichText:CreateMarkupEditControl(wndHost, strSkin, tProperties, tAddon) ##
+## GeminiRichText:CreateMarkupEditControl(wndHost [, strSkin] [, tProperties] [ , tAddon]) ##
 - wndHost = Place holder window, used to get Window Name, Anchors and Offsets, and Parent
 - strSkin = "Holo" or "Metal" -- not case sensitive
 - tProperties = table with special properties to be set, such as font face and color, or event methods
@@ -24,26 +23,39 @@ GeminiRichText = Apollo.GetPackage("GeminiRichText").tPackage
     }
 ```
 - tAddon = The addon that contains the methods defined in tProperties.
-- Returns wndMarkup, a reference to the new markup window.
+- **Returns** wndMarkup, a reference to the new markup window.
 
 ## GeminiRichText:SetText(wndMarkup, strText) ##
-- wndMarkup = The window you are setting text to. (a bypass for findign the correct children)
+- wndMarkup = The window you are setting text to.
 - strText = The text to be set.
 
 ## GeminiRichText:GetText(wndMarkup) ##
-- wndMarkup = The window you are getting text from. (a bypass for findign the correct children)
-- Returns strText, The text of the window.
+- wndMarkup = The window you are getting text from.
+- **Returns** strText = The text of the window.
+
+## GeminiRichText:CreateMarkupStyleEditor(wndHost [, tStyleData]) ##
+- wndHost = Place holder window, used to get Window Name, Anchors and Offsets, and Parent
+- tStyleData = A set of style data that conforms to the format in ParseMarkup method. If left out, a default set will be used.
+
+## GeminiRichText:GetStyleTable(wndStyleEditor) ##
+- wndSrtyleEditor = The style editor that is returnign the table.
+- **Returns** tStyleData = A table formatted for use with the ParseMarkup method.
+
+## GeminiRichText:SetStyleTable(wndStyleEditor, tStyleData) ##
+- wndSrtyleEditor = The style editor that you are setting the data to.
+- tStyleData = A table formatted for use with the ParseMarkup method.
 
 ## GeminiRichText:ParseMarkup(strText, tMarkupStyles) ##
 - strText = The text to be set.
 - tMarkupStyles = a table containing formatting for the markup styles.
+
 ```lua
-  tMarkupStyles = {
+  local tMarkupStyles = {
     {tag = "h1", font = "CRB_Interface14_BBO", color = "ffffffff", align = "Center"},
     {tag = "h2", font = "CRB_Interface12_BO", color = "ffffffff", align = "Left"},
     {tag = "h3", font = "CRB_Interface12_I", color = "ffffffff", align = "Left"},
     {tag = "p", font = "CRB_Interface12", color = "ffdddddd", align = "Left"},
     {tag = "li", font = "CRB_Interface12", color = "ffdddddd", align = "Left", bullet = "●", indent = "  "},
-  },
+  }
 ```
-- Returns strParsedText, The parsed text, formatted in XML markup.
+- **Returns** strParsedText, The parsed text, formatted in XML markup.
